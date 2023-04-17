af1368de52f472ede08e43995d51fd89e01e550b
voolyvex <1joeknapp@gmail.com>
2023-04-17 13:05:30 -0500

    fix: cors preflight allow origins error by updating settings + remove unused 'cars' app

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -5,8 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
 from .serializers import CommentSerializer
 
-# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
-
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
 
 AUTH_USER_MODEL = 'authentication.User'
 
@@ -40,7 +38,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'cars.apps.CarsConfig',  # This app is for example use only
     'comments.apps.CommentsConfig',
 ]
 
@@ -55,7 +52,24 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', 'http://127.0.0.1'
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [    'accept',    'accept-encoding',    'authorization',    'content-type',    'dnt',    'origin',    'user-agent',    'x-csrftoken',    'x-requested-with',]
+
+# CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'drf_jwt_backend.urls'
 
